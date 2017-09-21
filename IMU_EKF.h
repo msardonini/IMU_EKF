7f23c5f2ff82deb7e7459565975dd52fdbe6acfe
OscarJGV26 <31480609+OscarJGV26@users.noreply.github.com>
2017-09-21 17:32:38 +0100

    Update IMU_EKF.h

--- IMU_EKF.h
+++ IMU_EKF.h
@@ -3,13 +3,9 @@
 #include <math.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include "rtwtypes.h"
-#include "IMU_EKF_types.h"
 
 extern void IMU_EKF(float P[16], float q[4], const float Cov_info[3], const
                     float omega[3], float accel[3], float mag[3], float dt,
                     signed char ini, signed char use_mag);
-extern void IMU_EKF_initialize(void);
-extern void IMU_EKF_terminate(void);
 
 #endif
